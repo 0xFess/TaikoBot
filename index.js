f48599b9cf6b9ce2a3e4c909454c01caf6675cd2
0xFess <144781715+0xFess@users.noreply.github.com>
2024-06-29 01:12:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-const { web3, walletAddress } = require('./config/web3');
+const { web3, walletAddress, switchRpc } = require('./config/web3');
 const { lendAmount } = require('./src/module/minterest/lend');
 const { redeem } = require('./src/module/minterest/reedem');
 const { wrap } = require('./src/module/wrap/wrap');
 const { unwrap } = require('./src/module/wrap/unwrap');
-const AppConstant = require('./src/utils/constant');
 const BN = require('bn.js');
 
 function randomGasPrice() {
@@ -14,12 +13,45 @@ function randomGasPrice() {
     return randomGwei;
 }
 
+function randomIterations() {
+    return Math.random() < 0.5 ? 7 : 8; // Randomly choose between 7 or 8
+}
+
+async function getNonce(web3Instance) {
+    return await web3Instance.eth.getTransactionCount(walletAddress, 'pending');
+}
+
+async function executeTransaction(action, gasPriceWei, ...args) {
+    let web3Instance = web3;
+    let nonce = await getNonce(web3Instance);
+    while (true) {
+        try {
+            const gasLimit = new BN(800000);
+            const totalTxCost = gasLimit.mul(new BN(gasPriceWei));
+            const balanceWei = await web3Instance.eth.getBalance(walletAddress);
+            const balance = new BN(balanceWei);
+
+            if (balance.lt(totalTxCost)) {
+                console.log("Insufficient funds to cover the transaction cost. Transaction skipped.");
+                return;
+            }
+
+            return await action(...args, gasPriceWei.toString(), nonce);
+        } catch (error) {
+            console.error(`Error executing transaction: ${error.message}`);
+            console.log("Retrying...");
+            web3Instance = switchRpc(); // Switch to the next RPC URL
+            nonce = await getNonce(web3Instance);
+            await new Promise(resolve => setTimeout(resolve, 5000)); // Wait 5 seconds before retrying
+        }
+    }
+}
+
 async function main() {
     const lendRangeMin = 1.0;
     const lendRangeMax = 2.0;
-    const maxIterations = 7; 
+    const maxIterations = randomIterations();
     let iterationCount = 0;
-    let nonce = await web3.eth.getTransactionCount(walletAddress);
 
     while (iterationCount < maxIterations) {
         const gasPriceWei = randomGasPrice();
@@ -40,25 +72,29 @@ async function main() {
         // Lend
         let amount = Math.random() * (lendRangeMax - lendRangeMin) + lendRangeMin;
         amount = Math.floor(amount * 1_000_000);
-        let txHash = await lendAmount(amount, gasPriceWei.toString(), nonce++);
+        let txHash = await executeTransaction(lendAmount, gasPriceWei, amount);
+        if (!txHash) break;
         let txLink = `https://taikoscan.io/tx/${txHash}`;
         let amountDecimal = amount / 1_000_000;
         console.log(`Lend Transaction sent: ${txLink}, \nAmount: ${amountDecimal} USDC \nGwei: ${web3.utils.fromWei(gasPriceWei, 'gwei')} Gwei`);
 
         // Redeem
-        await redeem(gasPriceWei.toString(), nonce++);
+        txHash = await executeTransaction(redeem, gasPriceWei);
+        if (!txHash) break;
         
         // Wrap
         const wrapAmountMin = 0.0003;
         const wrapAmountMax = 0.0004;
         let wrapAmount = Math.random() * (wrapAmountMax - wrapAmountMin) + wrapAmountMin;
         wrapAmount = parseFloat(wrapAmount.toFixed(6));
-        txHash = await wrap(wrapAmount, gasPriceWei.toString(), nonce++);
+        txHash = await executeTransaction(wrap, gasPriceWei, wrapAmount);
+        if (!txHash) break;
         txLink = `https://taikoscan.io/tx/${txHash}`;
         console.log(`Wrap Transaction sent: ${txLink}, \nAmount: ${wrapAmount} ETH`);
 
         // Unwrap
-        txHash = await unwrap(wrapAmount, gasPriceWei.toString(), nonce++);
+        txHash = await executeTransaction(unwrap, gasPriceWei, wrapAmount);
+        if (!txHash) break;
         txLink = `https://taikoscan.io/tx/${txHash}`;
         console.log(`Unwrap Transaction sent: ${txLink}, \nAmount: ${wrapAmount} ETH`);
 
