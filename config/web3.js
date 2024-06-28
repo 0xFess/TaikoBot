f7c5b03593f2582579ef98e87d1a72e4b7e597cd
0xFess <144781715+0xFess@users.noreply.github.com>
2024-06-29 01:11:17 +0700

    Update web3.js

--- config/web3.js
+++ config/web3.js
@@ -1,7 +1,28 @@
 require('dotenv').config();
 const Web3 = require('web3');
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc.taiko.xyz'));
+const rpcUrls = [
+    'https://rpc.taiko.xyz',
+    'https://rpc.mainnet.taiko.xyz',
+    'https://rpc.ankr.com/taiko',
+    'https://rpc.taiko.tools',
+    'https://taiko.blockpi.network/v1/rpc/public'
+];
+
+let currentRpcIndex = 0;
+
+function getWeb3() {
+    const web3 = new Web3(new Web3.providers.HttpProvider(rpcUrls[currentRpcIndex]));
+    return web3;
+}
+
+function switchRpc() {
+    currentRpcIndex = (currentRpcIndex + 1) % rpcUrls.length;
+    console.log(`Switching to RPC: ${rpcUrls[currentRpcIndex]}`);
+    return getWeb3();
+}
+
+const web3 = getWeb3();
 const privateKey = process.env.PRIVATE_KEY;
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 const walletAddress = account.address;
@@ -11,5 +32,6 @@ console.log("Wallet Address:", walletAddress);
 module.exports = {
     web3,
     walletAddress,
-    privateKey
+    privateKey,
+    switchRpc
 };
