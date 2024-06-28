e47ac8880ba8d0af0969f6b0f0b306aff6d5f78f
0xFess <144781715+0xFess@users.noreply.github.com>
2024-06-28 23:44:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
 require('dotenv').config();
 const { web3, walletAddress } = require('./config/web3');
-const { lend} = require('./src/module/minterest/lend')
+const { lendAmount } = require('./src/module/minterest/lend');
 const { redeem } = require('./src/module/minterest/reedem');
 const { wrap } = require('./src/module/wrap/wrap');
 const { unwrap } = require('./src/module/wrap/unwrap');
 const AppConstant = require('./src/utils/constant');
+const BN = require('bn.js');
+
+function randomGasPrice() {
+    const minGwei = new BN(web3.utils.toWei('0.01005', 'gwei'));
+    const maxGwei = new BN(web3.utils.toWei('0.013', 'gwei'));
+    const randomGwei = minGwei.add(new BN(Math.floor(Math.random() * (maxGwei.sub(minGwei).toNumber()))));
+    return randomGwei;
+}
 
 async function main() {
-    let lastGasPrice = web3.utils.toWei(randomGasPrice(0.0100002, 0.015).toString(), 'gwei');
-    const maxIterations = 1;
+    const lendRangeMin = 1.0;
+    const lendRangeMax = 2.0;
+    const maxIterations = 7; 
     let iterationCount = 0;
+    let nonce = await web3.eth.getTransactionCount(walletAddress);
 
     while (iterationCount < maxIterations) {
-        const category = Math.random() < 0.5 ? 1 : 2;
+        const gasPriceWei = randomGasPrice();
 
-        if (category === 1) {
-            const lendRangeMin = 1.0;
-            const lendRangeMax = 2.0;
-            const gasLimit = AppConstant.maxgas;
-            const balance = await web3.eth.getBalance(walletAddress);
-            const gasPrice = web3.utils.toWei(randomGasPrice(0.0100002, 0.015).toString(), 'gwei');
-            const totalTxCost = gasLimit * gasPrice;
+        const balanceWei = await web3.eth.getBalance(walletAddress);
+        const balance = new BN(balanceWei);
+        const gasLimit = new BN(800000); // gas limit to 800,000
+        const totalTxCost = gasLimit.mul(gasPriceWei);
 
-            if (balance < totalTxCost) {
-                console.log("Insufficient funds to cover the transaction cost. Transaction skipped.");
-                continue;
-            }
+        console.log(`Gas Limit: ${gasLimit.toString()}, Gas Price: ${web3.utils.fromWei(gasPriceWei, 'gwei')} Gwei`);
+        console.log(`Total Tx Cost: ${web3.utils.fromWei(totalTxCost.toString(), 'ether')} ETH`);
 
-            let amount = Math.random() * (lendRangeMax - lendRangeMin) + lendRangeMin;
-            amount = Math.floor(amount * 1_000_000);
-            const txHash = await lend(amount, gasPrice);
-            const txLink = `https://taikoscan.io/tx/${txHash}`;
-            const amountDecimal = amount / 1_000_000;
-            console.log(`Transaction sent: ${txLink}, \nAmount: ${amountDecimal} USDC \nGwei: ${web3.utils.fromWei(gasPrice.toString(), 'gwei')}`);
-
-            lastGasPrice = gasPrice;
-            await new Promise(resolve => setTimeout(resolve, Math.random() * 80 + 50));
-            lastGasPrice = await redeem(lastGasPrice);
-        } else {
-            const wrapRangeMin = 0.003;
-            const wrapRangeMax = 0.005;
-
-            let amount = Math.random() * (wrapRangeMax - wrapRangeMin) + wrapRangeMin;
-            amount = parseFloat(amount.toFixed(6));
-            const txHash = await wrap(amount);
-            const txLink = `https://taikoscan.io/tx/${txHash}`;
-            console.log(`Transaction sent: ${txLink}, \nAmount: ${amount} ETH`);
-
-            await new Promise(resolve => setTimeout(resolve, Math.random() * 80 + 50));
-            const unwrapTxHash = await unwrap(amount);
-            const unwrapTxLink = `https://taikoscan.io/tx/${unwrapTxHash}`;
-            console.log(`Transaction sent: ${unwrapTxLink}, \nAmount: ${amount} ETH`);
+        if (balance.lt(totalTxCost)) {
+            console.log("Insufficient funds to cover the transaction cost. Transaction skipped.");
+            break;
         }
 
-        await new Promise(resolve => setTimeout(resolve, Math.random() * 80 + 50));
+        // Lend
+        let amount = Math.random() * (lendRangeMax - lendRangeMin) + lendRangeMin;
+        amount = Math.floor(amount * 1_000_000);
+        let txHash = await lendAmount(amount, gasPriceWei.toString(), nonce++);
+        let txLink = `https://taikoscan.io/tx/${txHash}`;
+        let amountDecimal = amount / 1_000_000;
+        console.log(`Lend Transaction sent: ${txLink}, \nAmount: ${amountDecimal} USDC \nGwei: ${web3.utils.fromWei(gasPriceWei, 'gwei')} Gwei`);
+
+        // Redeem
+        await redeem(gasPriceWei.toString(), nonce++);
+        
+        // Wrap
+        const wrapAmountMin = 0.0003;
+        const wrapAmountMax = 0.0004;
+        let wrapAmount = Math.random() * (wrapAmountMax - wrapAmountMin) + wrapAmountMin;
+        wrapAmount = parseFloat(wrapAmount.toFixed(6));
+        txHash = await wrap(wrapAmount, gasPriceWei.toString(), nonce++);
+        txLink = `https://taikoscan.io/tx/${txHash}`;
+        console.log(`Wrap Transaction sent: ${txLink}, \nAmount: ${wrapAmount} ETH`);
+
+        // Unwrap
+        txHash = await unwrap(wrapAmount, gasPriceWei.toString(), nonce++);
+        txLink = `https://taikoscan.io/tx/${txHash}`;
+        console.log(`Unwrap Transaction sent: ${txLink}, \nAmount: ${wrapAmount} ETH`);
+
         iterationCount++;
     }
 
@@ -61,7 +69,3 @@ async function main() {
 }
 
 main().catch(console.error);
-
-function randomGasPrice(min, max) {
-    return (Math.random() * (max - min) + min).toFixed(9);
-}
