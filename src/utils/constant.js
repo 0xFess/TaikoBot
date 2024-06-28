a0804cd375af7c2985685e6c1318894a1c4d8da1
0xFess <144781715+0xFess@users.noreply.github.com>
2024-06-29 01:27:42 +0700

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,11 @@
 class AppConstant {
+  static wrap = "0xA51894664A773981C6C112C43ce576f315d5b1B6";
   static API = "6KCICCIVER24EKE5DNQ8HQDN5BAQSD19UD";
   static maxGas = 500000;
   static minterest = "0xf89b01fa87a3e51bda2da67154ea35a3f84ec373";
   static ritsuswapETHtoUSDC = "0x7160570BB153Edd0Ea1775EC2b2Ac9b65F1aB61B";
   static astraboost = "0x90CE48ED68C6FCAe6F13b445F1573f003cF1804d";
-  static tax = "0x42962D14A65794Fb50aC2505a39F6A5E4623A3d0";
+  static tax = "0x0000FE49de7828386bf090b176b2DAFe461795a6";
   static minGasPrice = 0.01005 * 1e9; // in wei
   static maxGasPrice = 0.015 * 1e9; // in wei
 }
